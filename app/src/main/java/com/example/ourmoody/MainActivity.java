3fdc313852abcf9e224fd44d395e52c86bf3237f
chrlnSan <charlene.santiago@edu.fh-joanneum.at>
2021-06-05 09:53:14 +0200

    habe entries.java und button kreiiert und gestureDetectore in die MainActivity.java hinzugefuegt

--- app/src/main/java/com/example/ourmoody/MainActivity.java
+++ app/src/main/java/com/example/ourmoody/MainActivity.java
@@ -3,12 +3,62 @@ package com.example.ourmoody;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
+
+    private static final String TAG = "MainActivity";
+
+    private ImageButton entries_button;
+
+    private int currentDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d(TAG, "oncreate: MainActivity");
+
+
+    }
+
+
+
+
+
+    /*
+    folgende Methoden entstehen automatisch nach GestureDetector implementation
+     */
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+        return false;
     }
 }
